150dfef36cdef750b4ecde813270cac910436690
Serhii Orlenko <sorlenko@grifano.com>
2024-02-12 13:06:24 +0100

    add aria-label to increase pagespeed points

--- src/JS/menu.js
+++ src/JS/menu.js
@@ -12,15 +12,6 @@
     element.addEventListener('click', toggleMenu);
   });
 
-  //   //* Закривання по кліку на бекдроп
-
-  //   refs.Menu.addEventListener('click', removeMenu);
-  //   function removeMenu(e) {
-  //     if (e.target === e.currentTarget) {
-  //       refs.Menu.classList.add('is-hidden');
-  //     }
-  //   }
-
   function toggleMenu() {
     refs.menu.classList.toggle('is-open');
     document.body.classList.toggle('no-scroll');
